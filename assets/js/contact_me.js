a12b53eedc9cab647c342b4b04a66ae0d774851b
johnmark <johnmarkroco@dilc.upd.edu.ph>
2017-03-13 11:40:38 +0800

    Updated contact form

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -15,13 +15,9 @@ $(function() {
             var email = $("input#email").val();
             var contact = $("input#contact").val();
             var message = $("textarea#message").val();
-            var firstName = name; // For Success/Failure Message
-            // Check for white space in name for Success/Fail message
-            if (firstName.indexOf(' ') >= 0) {
-                firstName = name.split(' ').slice(0, -1).join(' ');
-            }
+
             $.ajax({
-                url: conturl,
+                url: '/digitalpenph-website/DigitalPenPhController/contactus',
                 type: "POST",
                 data: {
                     name: name,
@@ -29,21 +25,21 @@ $(function() {
                     email: email,
                     message: message
                 },
+                dataType: "json",
                 cache: false,
-                success: function(data) {
-                    alert(data);
-                    // Enable button & show success message
-                    $("#btnSubmit").attr("disabled", false);
-                    $('#success').html("<div class='alert alert-success'>");
-                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                        .append("</button>");
-                    $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
-                    $('#success > .alert-success')
-                        .append('</div>');
+                success: function() {
+                            //alert(stat.error);
+	                    $("#btnSubmit").attr("disabled", false);
+	                    $('#success').html("<div class='alert alert-success'>");
+	                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+	                        .append("</button>");
+	                    $('#success > .alert-success')
+	                        .append("<strong>Your message has been sent. </strong>");
+	                    $('#success > .alert-success')
+	                        .append('</div>');
 
-                    //clear all fields
-                    $('#contactForm').trigger("reset");
+	                    //clear all fields
+	                    $('#contactForm').trigger("reset");
 
                 },
                 error: function() {
@@ -51,10 +47,11 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Sorry " + name + ", it seems that my mail server is not responding. Please try again later!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
+                   // console.log(err);
                 },
             });
         },
